6e06201f239f0baf53b0734e5179a00e6bc3b999
ForU <shadowwing@163.com>
2016-10-10 11:33:07 +0800

    fetch

--- verifycode.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once './library/inc.php';
-include_once './library/cls.verifycode.php';
-$_vc = new ValidateCode(VERIFYCODE_WIDTH, VERIFYCODE_HEIGHT);
-$_vc->doimg();
-$_SESSION['verifycode'] = $_vc->getCode();
