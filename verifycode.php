3219648387cc2ef1a24af39f16531ddd350e2973
ForU <shadowwing@163.com>
2016-10-10 11:40:31 +0800

    update verifycode

---
+++ verifycode.php
@@ -0,0 +1,6 @@
+<?php
+include_once './library/inc.php';
+include_once './library/cls.verifycode.php';
+$_vc = new ValidateCode(VERIFYCODE_WIDTH, VERIFYCODE_HEIGHT);
+$_vc->doimg();
+$_SESSION['verifycode_admin'] = $_vc->getCode();//验证码保存到SESSION中
