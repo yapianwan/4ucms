6fa6e21dca4a970b5fb655476e1274f8e7801bd0
shadowwing <shadowwing@163.com>
2017-01-10 17:03:41 +0800

    fix verifycode.php

--- verifycode.php
+++ verifycode.php
@@ -4,4 +4,4 @@ include './library/cls.verifycode.php';
 
 $_vc = new VerifyCode(VERIFYCODE_WIDTH, VERIFYCODE_HEIGHT);
 $_vc->doimg();
-$_SESSION['verifycode_admin'] = $_vc->getCode();
+$_SESSION['verifycode'] = $_vc->getCode();
